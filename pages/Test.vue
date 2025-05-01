4c12631ebf7ae287b2a2f85faf03dc964566dfda
WMOUO <5316eictlws.2@gmail.com>
2025-05-01 23:51:36 +0800

    測試_2

--- pages/Test.vue
+++ pages/Test.vue
@@ -1,35 +1,44 @@
 <template>
     <div>
-        <Button @click="handlePrint" label="列印" icon="pi pi-print" class="p-button-info mb-4" />
-        <img id="print-img" :src="imageUrl" class="print-img" />
+        <Button @click="printImageOnly" label="列印圖" icon="pi pi-print" />
     </div>
 </template>
 
 <script setup lang="ts">
-  const imageUrl = '/test.png' // 注意：這是修改後的新圖
-const handlePrint = () => {
-    window.print()
-}
-</script>
+    const imageUrl = ref('test.png')
+    const printImageOnly = () => {
+    const printWindow = window.open('', '_blank')
+    if (!printWindow) return
 
-<style>
-@media print {
-    @page {
-        size: A4 landscape;
-        margin: 0;
-    }
-    body * {
-      visibility: hidden; /* 預設所有元素都隱藏 */
-    }
-    #print-img {
-        visibility: visible; /* 只讓圖片顯示 */
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        object-fit: contain;
-        display: block;
-    }
+        printWindow.document.write(`
+            <!DOCTYPE html>
+                <html>
+                    <head>
+                        <style>
+                            @page {
+                                size: A4 landscape;
+                                margin: 0;
+                            }
+                            html, body {
+                                margin: 0;
+                                padding: 0;
+                                height: 100%;
+                                width: 100%;
+                            }
+                            img {
+                                width: 100%;
+                                height: 100%;
+                                object-fit: contain;
+                                display: block;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <img src="test.png" onload="window.print(); window.close();" />
+                    </body> 
+                </html>
+    `)
+    printWindow.document.close()
 }
-</style>
+
+</script>
