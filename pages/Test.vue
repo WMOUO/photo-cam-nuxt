650259d8e852595af7154db2a0f030be7554545f
WMOUO <5316eictlws.2@gmail.com>
2025-05-02 00:12:57 +0800

    測試_3

--- pages/Test.vue
+++ pages/Test.vue
@@ -1,44 +1,45 @@
 <template>
     <div>
-        <Button @click="printImageOnly" label="列印圖" icon="pi pi-print" />
+        <Button @click="printA6Image" label="列印圖" icon="pi pi-print" />
     </div>
 </template>
 
 <script setup lang="ts">
     const imageUrl = ref('test.png')
-    const printImageOnly = () => {
-    const printWindow = window.open('', '_blank')
-    if (!printWindow) return
+    const printA6Image = () => {
+    const win = window.open('', '_blank')
+    if (!win) return
 
-        printWindow.document.write(`
-            <!DOCTYPE html>
-                <html>
-                    <head>
-                        <style>
-                            @page {
-                                size: A4 landscape;
-                                margin: 0;
-                            }
-                            html, body {
-                                margin: 0;
-                                padding: 0;
-                                height: 100%;
-                                width: 100%;
-                            }
-                            img {
-                                width: 100%;
-                                height: 100%;
-                                object-fit: contain;
-                                display: block;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <img src="test.png" onload="window.print(); window.close();" />
-                    </body> 
-                </html>
+    win.document.write(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <style>
+            @page {
+                size: 148mm 105mm;
+                margin: 0;
+            }
+            html, body {
+                margin: 0;
+                padding: 0;
+                height: 100%;
+                width: 100%;
+                overflow: hidden;
+            }
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                display: block;
+            }
+            </style>
+        </head>
+        <body>
+            <img src="/test.png" onload="window.print(); window.close();" />
+        </body>
+        </html>
     `)
-    printWindow.document.close()
-}
+    win.document.close()
+    }
 
 </script>
---
+++ pages/Test2.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <Button @click="printFullA4" label="列印A4圖" icon="pi pi-print" />
+    </div>
+</template>
+
+<script setup lang="ts">
+    const imageUrl = ref('test2.png')
+    const printFullA4 = () => {
+        const win = window.open('', '_blank')
+        if (!win) return
+
+        win.document.write(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <style>
+                @page {
+                    size: A4 landscape;
+                    margin: 0;
+                }
+                html, body {
+                    margin: 0;
+                    padding: 0;
+                    height: 100%;
+                    width: 100%;
+                    overflow: hidden;
+                }
+                img {
+                    width: 100%;
+                    height: 100%;
+                    object-fit: cover;
+                    display: block;
+                }
+                </style>
+            </head>
+            <body>
+                <img src="/test2.png" onload="window.print(); window.close();" />
+            </body>
+            </html>
+        `)
+        win.document.close()
+    }
+
+</script>
